c186e8e1a55b1b34f8609d1e07492a596afa5695
ronny macmaster <ronnymacmaster@gmail.com>
2016-02-19 16:01:30 -0600

    print items

--- PA3/Assignment3/Item.java
+++ PA3/Assignment3/Item.java
@@ -2,20 +2,18 @@ package Assignment3;
 
 public class Item 
 {
-//Declare variables for this class. Think about its type: public, protected or private?
-	protected String name;
-	protected float price;
-	protected int quantity;
-	protected int weight;
+	protected String name;  //item name
+	protected float price;  //item price
+	protected int quantity; //items quantity
+	protected int weight;   //
 
-// You will need a constructor (Why?). Create it here.
 	
-	Item(String n, float p, int q, int w)
+	Item(String name, float price, int quantity, int weight)
 	{
-		name = n;
-		price = p;
-		quantity = q;
-		weight = w;
+		this.name = name;
+		this.price = price;
+		this.quantity = quantity;
+		this.weight = weight;
 	}
 	
 	float calculatePrice () 
@@ -29,7 +27,16 @@ public class Item
 
 	void printItemAttributes () 
 	{
-		//Print all applicable attributes of this class
+		String itemString = ""; // Name, Price, and Quantity, Weight
+		
+		// build string
+		itemString += "Item: " + this.name + "\t";
+		itemString += "Price: " + this.calculatePrice() + "\t";
+		itemString += "Quantity: " + this.quantity + "\t";
+		itemString += "Weight: " + this.weight;
+		
+		// print string
+		System.out.println(itemString);
 	}
 
 }
