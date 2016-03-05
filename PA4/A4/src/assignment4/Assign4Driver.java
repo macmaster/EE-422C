9415ba5de3aff775d5215aa60823ec6b9ff6ca9f
ronny macmaster <ronnymacmaster@gmail.com>
2016-03-05 12:03:23 -0600

    doc refactor

--- PA4/A4/src/assignment4/Assign4Driver.java
+++ PA4/A4/src/assignment4/Assign4Driver.java
@@ -21,23 +21,23 @@ import java.util.regex.Matcher;
 import java.util.Iterator;
 
 public class Assign4Driver{
-	
+
 	private static Assignment4Interface wordLadderSolver;
 
 	public static void main(String[] args){
 		// Create dictionary
-		// Dictionary dict = new Dictionary("A4words.dat");	//takes the input from file of that name
+		// Dictionary dict = new Dictionary("A4words.dat"); //takes the input from
+		// file of that name
 		// System.out.println(dict);
 		// System.out.println("Dictionary Word count: " + dict.getCapacity());
 
 		// Create a word ladder solver object
-		
 
 		try{
 			// 1) Read dictionary and filename
 			String dictfile; // dictionary filename
 			String pairfile; // word pairs filename
-			
+
 			if(args.length != 2){ // invalid commandline arguments
 				Scanner input = new Scanner(System.in);
 				// prompt filenames
@@ -50,56 +50,43 @@ public class Assign4Driver{
 				dictfile = args[0];
 				pairfile = args[1];
 			}
-			
+
 			// 2) create a wordladdersolver object (includes graph and dictionary)
-			wordLadderSolver = new WordLadderSolver(dictfile);	
-			
+			wordLadderSolver = new WordLadderSolver(dictfile);
+
 			// 3) file input loop:
-				FileReader reader = new FileReader(pairfile);
-				BufferedReader fhand = new BufferedReader(reader);
-			
+			FileReader reader = new FileReader(pairfile);
+			BufferedReader fhand = new BufferedReader(reader);
+
 			String line;
 			Assign4Driver.printBanner();
-			while((line =  fhand.readLine()) != null){
+			while((line = fhand.readLine()) != null){
 				Pattern pattern = Pattern.compile("[a-zA-Z]{5}[\\s]+[a-zA-Z]{5}");
 				Matcher matcher = pattern.matcher(line);
-				
+
 				if(matcher.find()){
 					String[] words = matcher.group().split("[\\s]+");
 					String start = words[0];
-					String end  = words[1];
-					//centering the output
-					int remainspace = (65 - (26 + start.length() + end.length()));
-					for(int i = 0; i < (remainspace/2); i++){
-						System.out.print(" ");
-					}
-					
-					System.out.println("start word: " + start + "    end word: " + end);
+					String end = words[1];
+					System.out.println("\tstart word: " + start + "\tend word: " + end);
 					Assign4Driver.printLadder(start, end);
 					Assign4Driver.printBanner();
 				}
 				else{
 					System.err.println("Error: Input line does not contain a valid 5 letter word pair.");
-					//centering the output
-					int remainspace = 65 - (16 + line.length());
-					for(int i = 0; i < (remainspace/2); i++){
-						System.out.print(" ");
-					}
-					
 					System.err.println("Original input: " + line);
 					Assign4Driver.printBanner();
 					continue;
 				}
-					
-			}		
-			
-		}
-		catch(Exception err){
+
+			}
+
+		} catch(Exception err){
 			System.err.println("fail!!");
 		}
-		
+
 	}
-	
+
 	private static void printBanner(){
 		int bannerSize = 65;
 		for(int i = 0; i < bannerSize; i++){
@@ -107,21 +94,21 @@ public class Assign4Driver{
 		}
 		System.out.print("\n");
 	}
-	
+
 	private static void printLadder(String start, String end){
-	try{
-		List<String> wordLadder = wordLadderSolver.computeLadder(start, end);
-		Iterator ladderItr = wordLadder.iterator();
-		//iterate through wordladder
-		while(ladderItr.hasNext()){
-			System.out.println(ladderItr.next());			
+		try{
+			List<String> wordLadder = wordLadderSolver.computeLadder(start, end);
+			Iterator ladderItr = wordLadder.iterator();
+			// iterate through wordladder
+			while(ladderItr.hasNext()){
+				System.out.println(ladderItr.next());
+			}
+		} catch(Exception err){
+			System.err.println("No word ladder between: " + start + " and " + end + ".");
 		}
 	}
-	catch(Exception err){
-		System.err.println("No word ladder between: " + start + " and " + end + ".");}
-	}
-	
 
 }
 
-//they are giving us two inputs: first is dictionary, second contains the sets of two words
+// they are giving us two inputs: first is dictionary, second contains the sets
+// of two words
