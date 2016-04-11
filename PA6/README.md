a5fd29f4546b8ae7f870dbc23913d861a1a9b9e1
ronny macmaster <ronnymacmaster@gmail.com>
2016-04-11 17:05:12 -0500

    class templates

--- PA6/README.md
+++ PA6/README.md
@@ -2,11 +2,9 @@
 
 Group --  
 Authors : Cooper Raterink, Ronald Macmaster
-UT EID: , rpm953 
+UT EID: cdr2678, rpm953 
 Lab Session: Friday 2-3:30pm  
 git url : https://github.com/ronny-macmaster/EE-422C/tree/master/PA6  
-
-Analysis and Design : "doc/PA6 Analysis and Design"  
 
 JUnit Test files are in the test package. 
 You may need to add a JUnit jar to eclipse if you want to run them
--- PA6/src/assignment6/TicketClient.java
+++ PA6/src/assignment6/TicketClient.java
@@ -1,3 +1,15 @@
+/** TicketClient *********************************************
+ * Driver to manage the Word Ladder and Dictionary Classes.
+ * find word ladders between pairs from word file.
+ * check the data from the dictionary.
+ * print out the word ladders for each pair.
+ * 
+ * Section : F 2:00 - 3:30pm
+ * UT EID: cdr2678 ,rpm953
+ * @author Cooper Raterink, Ronald Macmaster
+ * @version 1.01 4/11/2016
+ ************************************************************/
+
 package assignment6;
 
 import java.io.BufferedReader;
