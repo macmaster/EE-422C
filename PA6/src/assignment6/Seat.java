0b0310eb29f4e57a6fa751d7a57ed50eeecdeb3b
Cooper Raterink <craterink@utexas.edu>
2016-04-11 18:43:23 -0500

    theatershow and seat classes

--- PA6/src/assignment6/Seat.java
+++ PA6/src/assignment6/Seat.java
@@ -1,8 +1,5 @@
 /** Seat *****************************************************
- * Driver to manage the Word Ladder and Dictionary Classes.
- * find word ladders between pairs from word file.
- * check the data from the dictionary.
- * print out the word ladders for each pair.
+ * Represents a seat at the Bates Recital Hall
  * 
  * Section : F 2:00 - 3:30pm
  * UT EID: cdr2678 ,rpm953
@@ -12,10 +9,109 @@
 
 package assignment6;
 
-public class Seat{
+/**
+ * Represents a Seat at the Bates Recital Hall.
+ * Can be compared to other Seats to determine which is the "better" seat
+ */
+public class Seat implements Comparable {
 
-	public Seat(){
-		// TODO Auto-generated constructor stub
+	/**
+	 * Enum classifying the section of the theater
+	 */
+	public enum Section { Left, Middle, Right }
+	
+	/**
+	 * Represents which section this Seat is in
+	 */
+	protected Section section;
+	
+	/**
+	 * The row this Seat is in
+	 */
+	protected int row;
+	
+	/**
+	 * This Seat's number
+	 */
+	protected int number;
+	
+	/**
+	 * Regex that only matches a valid row name.
+	 */
+	protected final String ValidRowNameRegex = "AA|[A-Z]";
+	
+	/**
+	 * Constructs this Seat with a certain section, row and number.
+	 * @param section Section of the theater
+	 * @param row Theater row
+	 * @param number Seat number
+	 */
+	public Seat(Section section, int row, int number){
+		this.section = section;
+		this.row = row;
+		this.number = number;
+	}
+	
+	/**
+	 * Converts a row number (1-27) to its row name:
+	 *  (1 = "A", 2 = "B", ... , 27 = "AA")
+	 * @param row Row number
+	 * @return Row name
+	 */
+	public String rowToRowName(int row) {
+		if(row > 27 || row < 1)
+			return null;
+		else
+			return (row <= 26) ? String.valueOf('A' + row - 1) : "AA";
+	}
+	
+	/**
+	 * Converts a row's name ("A" thru "AA") to its corresponding row number (1-27).
+	 * @param rowName Row's name
+	 * @return If valid, the row's number. Otherwise, -1.
+	 */
+	public int rowNameToRow(String rowName) {
+		//check if row name is invalid
+		if(rowName == null || !rowName.matches(ValidRowNameRegex)) {
+			return -1;
+		}
+		else if (rowName.length() == 1) {
+			return (int) (rowName.charAt(0) - 'A' + 1);
+		}
+		else
+			return 27;
+	}
+	
+	@Override
+	/**
+	 * Comparable by the "value" of the seat:
+	 * Front-middle seats are best, then front-sides, then the next-row-middle, etc
+	 * @param otherSeatObj Seat to compare this seat to
+	 * @return -1 if this seat is the "better" Seat,
+	 * 			1 if the other seat is "better",
+	 * 			0 if they are equal
+	 */
+	public int compareTo(Object otherSeatObj) {
+		Seat otherSeat = (Seat) otherSeatObj; 
+		
+		//Compare rows first
+		if(row != otherSeat.row)
+			return (row < otherSeat.row) ? -1 : 1;
+		
+		//If in the same row, compare sections
+		if (section != otherSeat.section) {
+			if(section == Section.Middle)
+				return 1;
+			else if(otherSeat.section == Section.Middle) {
+				return -1;
+			}
+		}
+		
+		//If seats are in same row and similarly-valued sections, 
+		//they are of equal value
+		return 0;
 	}
 
 }
+
+
--- PA6/src/assignment6/TheaterShow.java
+++ PA6/src/assignment6/TheaterShow.java
@@ -12,10 +12,99 @@
 
 package assignment6;
 
+import java.util.PriorityQueue;
+
+/**
+ * Class representing a single show at Bates Recital Hall. 
+ * It handles keeping track of available seats and
+ * finding seats for customers.
+ */
 public class TheaterShow{
 
+	/**
+	 * Queue of seats at the theater, sorted from best to worst
+	 */
+	protected PriorityQueue<Seat> availableSeats;
+	
+	protected final int RowsAtBatesHall = 27;
+	
+	/**
+	 * Constructs a new TheaterShow object - with all seats available
+	 */
 	public TheaterShow(){
-		// TODO Auto-generated constructor stub
+		initSeats();
+	}
+	
+	/**
+	 * Determines whether a seat is available at this show.
+	 * @return True is a seat is available for the show, False otherwise.
+	 */
+	public boolean isSeatAvailable() {
+		return !availableSeats.isEmpty();
+	}
+	
+	/**
+	 * @return Returns the best available Seat
+	 * @throws NoSeatAvailableException If there's no seat available
+	 */
+	public Seat getBestAvailableSeat() throws NoSeatAvailableException {
+		if(isSeatAvailable())
+			return availableSeats.remove();
+		else
+			throw new NoSeatAvailableException();
+	}
+	
+	/**
+	 * Generates the available seats queue, containing all seats
+	 * at Bates Recital Hall sorted automatically by best to worst.
+	 */
+	private void initSeats() {
+		availableSeats = new PriorityQueue<Seat>();
+		//first row through last row
+		for(int row = 1; row <= RowsAtBatesHall; row++) {
+			//left section to right section
+			for(Seat.Section sect : Seat.Section.values()) {
+				switch (sect) {
+				//left section has seat numbers 122-128 
+				//except for last row which has seats 127-128
+				case Left:
+					int seatStart = (row < 27) ? 122 : 127;
+					for(int seatNum = seatStart; seatNum <= 128; seatNum++) {
+						availableSeats.add(new Seat(sect, row, seatNum));
+					}
+				//middle section has seat numbers 108-121
+				//except has no last 3 rows
+				case Middle:
+					if(row < 25) {
+						for(int seatNum = 108; seatNum <= 121; seatNum++) {
+							availableSeats.add(new Seat(sect, row, seatNum));
+						}
+					}
+				//right section has seat numbers:
+				//no rows 1-2
+				//101-106 for row 3
+				//101-107 for rows 4-26
+				//101-104 and 116-118 for last row
+				case Right:
+					if(row > 2 && row != 27) {
+						int seatStop = (row != 3)? 107 : 106;
+						for(int seatNum = 101; seatNum <= seatStop; seatNum++) {
+							availableSeats.add(new Seat(sect, row, seatNum));
+						}
+					}
+					if(row == 27) {
+						for(int seatNum = 101; seatNum <= 104; seatNum++) {
+							availableSeats.add(new Seat(sect, row, seatNum));
+						}
+						for(int seatNum = 116; seatNum <= 118; seatNum++) {
+							availableSeats.add(new Seat(sect, row, seatNum));
+						}
+					}
+				}
+			}
+		}
+		
+		
 	}
 
 }
