efcae58074c28e13ecf59306818155f8f8fc496f
ronny macmaster <ronnymacmaster@gmail.com>
2016-04-27 00:20:52 -0500

    more relative functionality. now for the hard part....

--- PABonus/src/main/SecretCode.java
+++ PABonus/src/main/SecretCode.java
@@ -1,3 +1,12 @@
+/** SecretCode ********************************************
+ * Generates a random secret code answer
+ * default secret code length is 4
+ * 
+ * Section : F 2:00 - 3:30pm
+ * UT EID: cdr2678 ,rpm953
+ * @author Cooper Raterink, Ronald Macmaster
+ * @version 1.01 4/25/2016
+ ************************************************************/
 package main;
 
 import java.awt.Color;
@@ -5,21 +14,47 @@ import java.awt.Font;
 import java.awt.Graphics2D;
 
 public class SecretCode extends GraphicalCode {
-
+	
+	/** default secret code length */
+	private static int length = 4;
+	
+	/**
+	 * Standard constructor with no length specified
+	 * @param x coordinate x for the secret code pegs
+	 * @param y coordinate y for the secret code pegs
+	 * @param radius secret code peg radius
+	 */
 	public SecretCode(int x, int y, int radius) {
-		super(new Code(4), x, y, radius);
+		super(new Code(length), x, y, radius);
 	}
 	
+	/**
+	 * Standard constructor with a length field specified
+	 * @param x coordinate x for the secret code pegs
+	 * @param y coordinate y for the secret code pegs
+	 * @param radius secret code peg radius
+	 * @param length secret code length
+	 */
+	public SecretCode(int x, int y, int radius, int length) {
+		super(new Code(length), x, y, radius);
+	}
+	
+	/**
+	 * override the draw method to 
+	 * draw the graphical pegs with question marks 
+	 */
 	@Override
 	public void draw(Graphics2D g) {
+		// draw Secret Code pegs
 		super.draw(g);
-		g.setFont(new Font(
-				"Comic Sans",
-				Font.PLAIN,
-				24));
+		
+		// draw question marks
 		g.setColor(Color.black);
+		g.setFont(new Font("Comic Sans", Font.PLAIN, 24));
 		for(GraphicalPeg peg : pegs) {
-			g.drawString("?", peg.x + 12 * peg.radius / 16, peg.y + 22 * peg.radius / 16);
+			int pegX = peg.x + 12 * peg.radius / 16;
+			int pegY = peg.y + 22 * peg.radius / 16;
+			g.drawString("?", pegX, pegY);
 		}
 	}
 
